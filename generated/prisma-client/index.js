ffa7fc157be177e897fc8ecb63fc50056e52050a
IbrahimTarabay <ibrahimesalah69@gmail.com>
2020-03-20 18:01:14 +0200

    update

--- generated/prisma-client/index.js
+++ generated/prisma-client/index.js
@@ -21,6 +21,6 @@ exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
   endpoint: `
-  https://rocky-sierra-00708.herokuapp.com/style88/dev`
+  https://style88-0b0f335ce0.herokuapp.com//style88/dev`
 });
 exports.prisma = new exports.Prisma();
