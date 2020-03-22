310199fe0e7d33556d85abbce6910bbe575eb04f
Ibrahim_Tarabay <32605959+IbrahimTarabay@users.noreply.github.com>
2020-03-22 21:35:41 +0200

    Update index.js

--- generated/prisma-client/index.js
+++ generated/prisma-client/index.js
@@ -21,6 +21,6 @@ exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
   endpoint: `
-  https://style88-0b0f335ce0.herokuapp.com//style88/dev`
+  https://style88-0b0f335ce0.herokuapp.com/style88/dev`
 });
 exports.prisma = new exports.Prisma();
